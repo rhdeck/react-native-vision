7785794f027e8009ac64c6655151bff7010ff830
Ray Deck <ray@raydeck.com>
2018-05-06 10:18:28 -0400

    Removing other legacy m file

--- ios/RHDVision/RHDVisionDelegate.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-@interface RCT_EXTERN_MODULE(RHDVisionModule, RCTEventEmitter)
-RCT_EXTERN_METHOD(attach)
-RCT_EXTERN_METHOD(saveFrame:(NSString *)description resolve: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(detectFaces: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(trackObject: (NSString *)name dict:(NSDictionary *)dict resolve: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-@end
