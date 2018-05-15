37e77104ae14c6722b49f50e20506cb91f225eea
Ray Deck <ray@raydeck.com>
2018-05-15 16:54:02 -0400

    Making wrapper what it is today

--- wrapper.js
+++ wrapper.js
@@ -1,12 +1,32 @@
 import React, { createContext, Component } from "react";
 import PropTypes from "prop-types";
-import Module, { detach, start, stop } from "./module";
+import Module, {
+  detach,
+  start,
+  stop,
+  detectFacesOnce,
+  removeDetectFaces,
+  detectFaces,
+  removeRegion,
+  setRegion,
+  trackObject,
+  removeTrackObject,
+  getImageDimensions
+} from "./module";
 const { Provider, Consumer } = createContext(null);
 class Wrapper extends Component {
   state = {
     isStarted: false,
-    isCameraFront: false
+    isCameraFront: false,
+    onDetectedFaces: null,
+    fixedRegions: {},
+    trackedObjects: {},
+    calculatedRegions: {},
+    imageDimensions: { height: 0, width: 0 }
   };
+  componentDidMount() {
+    this.getProviderValue();
+  }
   componentWillUnmount() {
     if (this.state.connected) {
       stop();
@@ -14,33 +34,202 @@ class Wrapper extends Component {
   }
   static getDerivedStateFromProps(nextProps, prevState) {
     var ret = prevState;
-    console.log(ret);
+    if (!ret.todos) ret.todos = [];
     if (nextProps.isCameraFront != prevState.isCameraFront) {
       ret.isCameraFront = nextProps.isCameraFront;
-      if (prevState.isStarted) start(ret.isCameraFront);
+      if (prevState.isStarted) {
+        start(ret.isCameraFront);
+        ret.todos.push({ key: "getImageDimensions" });
+      }
     }
     if (nextProps.isStarted != prevState.isStarted) {
       ret.isStarted = nextProps.isStarted;
-      nextProps.isStarted ? start(ret.isCameraFront) : stop();
+      if (nextProps.isStarted) {
+        start(ret.isCameraFront);
+        ret.todos.push({ key: "getImageDimensions" });
+      } else stop();
     }
+    if (nextProps.onDetectedFaces != prevState.onDetectedFaces) {
+      if (nextProps.onDetectedFaces) {
+        //Turn it on
+        console.log("Activating detectFaces");
+        detectFaces("", nextProps.onDetectedFaces);
+        ret.todos.push({ key: "getImageDimensions" });
+      } else {
+        //Turn it off
+        console.log("removing detectfaces");
+        removeDetectFaces("");
+        ret.todos.push({ key: "getImageDimensions" });
+      }
+      ret.onDetectedFaces = nextProps.onDetectedFaces;
+    }
+    if (nextProps.trackedObjects != prevState.trackedObjects) {
+      if (nextProps.trackedObjects)
+        Object.keys(nextProps.trackedObjects).forEach(k => {
+          const v = nextProps.trackedObjects[k];
+          if (!prevState.trackedObjects[k]) {
+            if (!ret.todoObjects) ret.todoObjects = {};
+            ret.todoObjects[k] = nextProps.trackedObjects[k];
+          }
+          if (
+            JSON.stringify(prevState.trackedObjects[k]) != JSON.stringify(v)
+          ) {
+            if (!ret.todoObjects) ret.todoObjects = {};
+            ret.calculatedRegions[k] = v;
+            ret.todoObjects[k] = v;
+          }
+        });
+      Object.keys(prevState.trackedObjects).forEach(k => {
+        const v = prevState.trackedObjects[k];
+        if (!nextProps.trackedObjects[k]) {
+          removeTrackObject("", k);
+          delete ret.calculatedRegions[k];
+        }
+      });
+      ret.todos.push({ key: "getImageDimensions" });
+      ret = nextProps.trackedObjects;
+    }
+    if (nextProps.regions != prevState.fixedRegions) {
+      var shouldChange = false;
+      Object.keys(prevState.fixedRegions).forEach(k => {
+        if (!nextProps.regions[k]) {
+          shouldChange = true;
+          removeRegion(k);
+        }
+      });
+      if (shouldChange) {
+        ret.fixedRegions = nextProps.regions;
+        Object.keys(ret.fixedRegions).forEach(k => {
+          setRegion(k, ret.fixedRegions[k]);
+        });
+      }
+    }
+    if (ret && ret.todos && ret.todos.length == 0) delete ret.todos;
     return ret;
+  }
+  async getImageDimensions() {
+    console.log("Getting image dimensions");
+    const dims = await getImageDimensions();
+    console.log("Got image dimensions!", dims);
+    if (dims.height == 0) {
+      setTimeout(() => {
+        this.getImageDimensions();
+      }, 1000);
+    } else {
+      this.setState(({ imageDimensions }) => {
+        if (
+          dims &&
+          (dims.height != imageDimensions.height ||
+            dims.width != imageDimensions.width)
+        )
+          return { imageDimensions: dims };
+      });
+    }
+  }
+  manageTodo() {
+    const todos = this.state.todos;
+    if (todos) {
+      todos.forEach(v => {
+        const k = v.key;
+        if (this[k]) return this[k](v);
+      });
+      this.setState({ todos: null });
+    }
+    const todo = this.state.todoObjects;
+    if (todo) {
+      Object.keys(todo).forEach(k => {
+        (async () => {
+          try {
+            console.log("Adding trackobject");
+            await removeTrackObject("", k);
+            await trackObject("", k, todo[k], newRect => {
+              console.log("Tracked change in ", k);
+              this.setState(({ calculatedRegions }) => {
+                const newcalcregions = { ...calculatedRegions, [k]: newRect };
+                console.log(
+                  "Newcalcregions",
+                  newcalcregions,
+                  calculatedRegions
+                );
+                return {
+                  calculatedRegions: newcalcregions
+                };
+              });
+            });
+          } catch (e) {
+            console.log("TRACKOBJECT FAIL", k, todo[k], e);
+          }
+        })();
+      });
+      this.setState({ todoObjects: null }, () => {
+        this.getProviderValue();
+      });
+    }
+  }
+  componentDidUpdate() {
+    this.manageTodo();
   }
   providerValue = {};
   getProviderValue() {
     //Check state
+    isChanged = false;
     //Regions
+    if (Object.keys(this.providerValue).length == 0) {
+      isChanged = true;
+    } else if (
+      this.providerValue &&
+      this.providerValue.imageDimensions &&
+      (this.providerValue.imageDimensions.height !=
+        this.state.imageDimensions.height ||
+        this.providerValue.imageDimensions.width !=
+          this.state.imageDimensions.width)
+    ) {
+      isChanged = true;
+    } else if (Object.keys(this.state.calculatedRegions).length) {
+      isChanged = true;
+    } else {
+      Object.keys(this.state.fixedRegions).forEach(k => {
+        const v = this.state.fixedRegions[k];
+        if (
+          !providerValue.regions[k] ||
+          JSON.stringify(providerValue.regions[k] != JSON.stringify(v))
+        ) {
+          isChanged = true;
+        }
+      });
+    }
+    if (isChanged) {
+      this.providerValue = {
+        imageDimensions: this.state.imageDimensions,
+        isCameraFront: this.state.isCameraFront,
+        regions: {
+          "": null,
+          ...this.state.fixedRegions,
+          ...this.state.calculatedRegions
+        }
+      };
+      if (typeof this.props.onRegionsChanged == "function") {
+        this.props.onRegionsChanged(this.providerValue.regions);
+      }
+    }
     //If state mutated relative to providerValue, change providerValue
     return this.providerValue;
   }
   render() {
-    const base = <Provider value={this.getProviderValue()} />;
-    if (typeof this.props.children == "function") {
-      return [base, <Region id="">{this.props.children}</Region>];
-    } else return base;
+    return (
+      <Provider value={this.providerValue}>{this.props.children}</Provider>
+    );
   }
 }
 Wrapper.propTypes = {
   isStarted: PropTypes.bool.isRequired,
-  isCameraFront: PropTypes.bool.isRequired
+  isCameraFront: PropTypes.bool.isRequired,
+  onDetectedFaces: PropTypes.func,
+  trackedObjects: PropTypes.object,
+  regions: PropTypes.object,
+  onRegionsChanged: PropTypes.func
+};
+Wrapper.detectFaces = async () => {
+  return await detectFacesOnce("");
 };
 export { Wrapper, Consumer };
