08bc22bf46bb50c46ae9de3f7b76aa42c87081f3
Ray Deck <ray@raydeck.com>
2018-05-17 20:45:13 -0400

    Fixing classifier and generator support

--- region.js
+++ region.js
@@ -4,16 +4,17 @@ import { Consumer } from "./wrapper";
 import * as Module from "./module";
 class Region extends Component {
   state = {
-    classifiers: {},
-    generators: {},
-    generics: {},
-    bottlenecks: {},
+    classifiers: null,
+    generators: null,
+    generics: null,
+    bottlenecks: null,
     classifications: {},
     genericResults: {},
     frameListener: null
   };
   static getDerivedStateFromProps(nextProps, prevState) {
     var ret = prevState;
+    if (!ret.todos) ret.todos = {};
     if (prevState.frameListener != nextProps.onFrameCaptured) {
       console.log("FRAME DIFF");
       if (nextProps.onFrameCaptured) {
@@ -23,23 +24,31 @@ class Region extends Component {
           nextProps.region,
           nextProps.frameDisposition ? nextProps.frameDisposition : "file",
           body => {
-            console.log("I AM FRAME CAPTURE BACK");
             nextProps.onFrameCaptured(body);
           }
         );
       } else {
-        console.log("I AM TURNING OFF FRAME CAPTURE");
         Module.removeSaveFrame(nextProps.region);
       }
       ret.frameListener = nextProps.onFrameCaptured;
     }
     if (nextProps.classifiers != prevState.classifiers) {
-      //compare the two
-      if (
-        JSON.stringify(nextProps.classifiers) !=
-        JSON.stringify(prevState.classifiers)
-      ) {
-        if (nextProps.classifiers)
+      if (nextProps.classifiers)
+        if (Array.isArray(nextProps.classifiers))
+          nextProps.classifiers.forEach(obj => {
+            const url = obj.url;
+            const maxCount = obj.max;
+            if (
+              prevState.classifiers &&
+              prevState.classifiers.filter(o => {
+                return o.url == url && o.max == maxCount;
+              }).length
+            )
+              return;
+            if (!ret.todos.addMLClassifiers) ret.todos.addMLClassifiers = [];
+            ret.todos.addMLClassifiers.push(obj);
+          });
+        else
           Object.keys(nextProps.classifiers).forEach(k => {
             const maxCount = nextProps.classifiers[k];
             if (
@@ -47,36 +56,65 @@ class Region extends Component {
               prevState.classifiers[k] == maxCount
             )
               return;
-            Module.applyMLClassifier(
-              nextProps.region,
-              nextProps.region,
-              k,
-              maxCount,
-              newClassifications => {
-                this.setState(({ classifications }) => {
-                  return {
-                    classifications: {
-                      ...classifications,
-                      k: newClassifications
-                    }
-                  };
-                });
-              }
-            );
+            if (!ret.todos.addMLClassifiers) ret.todos.addMLClassifiers = [];
+            ret.todos.addMLClassifiers.push({ url: k, max: maxCount });
           });
-        Object.keys(prevState.classifiers).forEach(k => {
-          if (
-            !nextProps.classifiers ||
-            typeof nextProps.classifiers[k] == "undefined"
-          ) {
-            Module.removeML(nextProps.region, k);
-            this.setState(({ classifications }) => {
-              return { ...classifications, k: null };
-            });
-          }
-        });
-        ret.classifiers = nextProps.classifiers;
-      }
+      if (prevState.classifiers)
+        if (Array.isArray(prevState.classifiers))
+          prevState.classifiers.forEach(obj => {
+            if (
+              !nextProps.classifiers ||
+              !nextProps.classifiers.filter(nobj => {
+                return nobj.url == obj.url;
+              }).length
+            ) {
+              Module.removeML(nextProps.region, obj.url);
+              delete ret.classifications[obj.url];
+            }
+          });
+        else
+          Object.keys(prevState.classifiers).forEach(k => {
+            if (
+              !nextProps.classifiers ||
+              typeof nextProps.classifiers[k] == "undefined"
+            ) {
+              Module.removeML(nextProps.region, k);
+              delete ret.classifications[k];
+            }
+          });
+      ret.classifiers = nextProps.classifiers;
+    }
+    if (nextProps.generators != prevState.generators) {
+      if (nextProps.generators)
+        if (Array.isArray(nextProps.generators))
+          nextProps.generators.forEach(obj => {
+            const url = obj.url;
+            const type = obj.type;
+            if (
+              prevState.generators &&
+              prevState.generators.filter(o => {
+                const ret = o.url == url && o.type == type;
+                return ret;
+              }).length
+            )
+              return;
+            if (!ret.todos.addMLGenerators) ret.todos.addMLGenerators = [];
+            ret.todos.addMLGenerators.push(obj);
+          });
+      if (prevState.generators)
+        if (Array.isArray(prevState.generators))
+          prevState.generators.forEach(obj => {
+            if (
+              !nextProps.generators ||
+              !nextProps.generators.filter(nobj => {
+                return nobj.url == obj.url;
+              }).length
+            ) {
+              Module.removeML(nextProps.region, obj.url);
+              delete ret.generators;
+            }
+          });
+      ret.generators = nextProps.generators;
     }
     if (nextProps.bottlenecks != prevState.bottlenecks) {
       //Update bottlenecks - keys are the bottleneck URLS, values are an object of keys generators, generics, classifiers - examine them all
@@ -95,8 +133,60 @@ class Region extends Component {
     }
     return ret;
   }
+  componentDidUpdate() {
+    this.manageTodo();
+  }
+  componentDidMount() {
+    this.manageTodo();
+  }
+  manageTodo() {
+    if (this.state.todos) {
+      Object.keys(this.state.todos).forEach(k => {
+        if (typeof this[k] == "function") this[k](this.state.todos[k]);
+        else console.log("No todo function for key ", k);
+      });
+      this.setState({ todos: null });
+    }
+  }
+  addMLClassifiers(classifiers) {
+    classifiers.forEach(obj => {
+      const url = obj.url;
+      const maxCount = obj.max;
+      Module.applyMLClassifier(
+        this.props.region,
+        url,
+        maxCount,
+        newClassifications => {
+          this.setState(({ classifications }) => {
+            return {
+              classifications: {
+                ...classifications,
+                [url]: newClassifications
+              }
+            };
+          });
+        }
+      );
+    });
+  }
+  addMLGenerators(generators) {
+    generators.forEach(o => {
+      const url = o.url;
+      Module.applyMLGenerator(this.props.region, url, o.type, data => {
+        if (Array.isArray(this.state.generators))
+          this.state.generators
+            .filter(obj => {
+              return obj.url == url;
+            })
+            .forEach(obj => {
+              obj.callback(data);
+            });
+        else this.state.generators[url].callback(data);
+      });
+    });
+  }
   render() {
-    return (
+    return this.props.children ? (
       <Consumer>
         {value => {
           if (!value) return;
@@ -111,7 +201,7 @@ class Region extends Component {
           return this.props.children(regionInfo);
         }}
       </Consumer>
-    );
+    ) : null;
   }
 }
 Region.propTypes = {
@@ -120,7 +210,7 @@ Region.propTypes = {
   generators: PropTypes.array,
   generics: PropTypes.array,
   bottlenecks: PropTypes.object,
-  children: PropTypes.func.isRequired,
+  children: PropTypes.func,
   onFrameCaptured: PropTypes.func,
   frameDisposition: PropTypes.string
 };
