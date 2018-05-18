1bcca78a2a7a4b32b7a536606144dbb4838f6f29
Ray Deck <ray@raydeck.com>
2018-05-17 20:44:07 -0400

    Setting up imageview for success.

--- imageview.js
+++ imageview.js
@@ -2,19 +2,14 @@ import { requireNativeComponent, NativeModules } from "react-native";
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const NativeVision = requireNativeComponent("RHDVisionImageView", RNVision);
+const NativeVision = requireNativeComponent("RHDVisionImageView", RNVImageView);
 
-class RNVision extends Component {
-  constructor(props) {
-    super(props);
-    if (typeof props.id == "undefined")
-      throw Error("ImageView requires an id prop");
-  }
+class RNVImageView extends Component {
   render() {
     return <NativeVision {...this.props} />;
   }
 }
-RNVision.propTypes = {
-  id: PropTypes.string
+RNVImageView.propTypes = {
+  id: PropTypes.string.isRequired
 };
-export default RNVision;
+export default RNVImageView;
