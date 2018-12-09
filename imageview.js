16b91533afb33dd04fc4d434df08a9e9cf6d94e1
Ray Deck <ray@raydeck.com>
2018-12-09 10:26:42 -0500

    Allowing resizemode to come from the prop instead of depending on style.

--- imageview.js
+++ imageview.js
@@ -1,16 +1,17 @@
-import { requireNativeComponent, NativeModules } from "react-native";
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { RHDVisionImageView } from "./RNSwiftBridge";
-// const NativeVision = requireNativeComponent("RHDVisionImageView", RNVImageView);
-
 class RNVImageView extends Component {
   render() {
     return (
       <RHDVisionImageView
         {...this.props}
         resizeMode={
-          this.props.style.resizeMode ? this.props.style.resizeMode : "cover"
+          this.props.resizeMode
+            ? this.props.resizeMode
+            : this.props.style.resizeMode
+            ? this.props.style.resizeMode
+            : "cover"
         }
       />
     );
