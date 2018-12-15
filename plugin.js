0598c9889db146f88319a6ce3dd660ad6edc512a
Ray Deck <ray@raydeck.com>
2018-12-15 07:04:39 -0500

    Moving to scoped xcode package

--- plugin.js
+++ plugin.js
@@ -1,5 +1,5 @@
 const { spawnSync } = require("child_process");
-const xcode = require("xcode");
+const xcode = require("@raydeck/xcode");
 const { join, basename, isAbsolute } = require("path");
 const {
   existsSync,
